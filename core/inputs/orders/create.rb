f3df2ee347c846a69c86fa445dabfe5cebefd6bc
Timur Platonov <tpltnz@gmail.com>
2020-05-14 02:13:18 +0300

    add dry-validation rules

--- core/inputs/orders/create.rb
+++ core/inputs/orders/create.rb
@@ -3,25 +3,49 @@ require_relative '../base'
 module Inputs
   module Orders
     class Create < Base
-      # Dry::Schema.Params - http params (strings as keys, strings as values)
-      # Dry::Schema.JSON - json params
-      #
-      # key must present && value is nullable
-      # required(:comment).maybe(:string)
-      #
-      # key is optional
-      # optional(:comment)
-      #
-      # value may be ''
-      # required(:comment).value(:string)
+      # schema - no coercions (Dry::Schema)
+      # json - Dry::Schema.JSON
+      # params - Dry::Schema.Params (http params coercions: strings as keys, strings as values)
 
       json do
         # key must present && value must present
         required(:comment).filled(:string)
+
+        # key must present && value is nullable
+        # required(:comment).maybe(:string)
+        #
+        # key is optional
+        # optional(:comment)
+        #
+        # value may be ''
+        # required(:comment).value(:string)
       end
 
-      # TODO:
-      # https://github.com/dry-rb/dry-validation
+      # rule(contact: [:birthday, ..]) - execute if schema for path succeed
+      # rule(..).each - apply to array items
+      rule(:comment) do
+        if value == 'UNEXPECTED'
+          # select first key from rule() args
+          key.failure('UNEXPECTED')
+
+          # key(:x).failure('x')
+        end
+
+        # key? - key comment present?
+        # value - eq to input[:comment]
+        # values - eq to input
+      end
+
+      # rule do
+      #   base.failure('base error => {nil=>["base error"]}')
+      # end
+
+      register_macro(:string_validator) do |macro:|
+        arg0 = macro.args[0]
+        key.failure('42') if arg0 != 42
+      end
+
+      rule(:comment).validate(string_validator: 42)
     end
   end
 end
